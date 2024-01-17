2a881443229aefef77221ff3c6e30fef5130dd4a
Fabio <cassisafabio97@gmail.com>
2024-01-17 14:20:04 +0100

    gsap for header, smooth animation on hover menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,23 @@
 //Header of the website
 import { Logo } from "../assets/SVGassets"
+import { gsap } from "gsap";
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollToPlugin);
 
 export const Header = () => {
-  
+  const scrollToSection = (sectionId) => {
+    gsap.to(window, { duration: 1.5, scrollTo: `#${sectionId}`, ease: 'power2.inOut' });
+  };
   
   return (
     <header className="header-menu">
         <nav className="header-menu-nav">
-          <a href="#a">ABOUT US</a>
-          <a href="#s">SERVICES</a>
+          <a onClick={() => scrollToSection('a')} className="menu-element">ABOUT US</a>
+          <a onClick={() => scrollToSection('s')} className="menu-element">SERVICES</a>
           <Logo />
-          <a href="#j">JOIN US</a>
-          <a href="#c">CONTACTS</a>
+          <a onClick={() => scrollToSection('j')} className="menu-element">JOIN US</a>
+          <a onClick={() => scrollToSection('c')} className="menu-element">CONTACTS</a>
         </nav>
     </header>
   )
