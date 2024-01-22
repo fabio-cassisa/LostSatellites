e73e72b73da00a8cbf87cbb016237e37045fe142
Fabio <cassisafabio97@gmail.com>
2024-01-22 18:36:37 +0100

    added the onClick fun to redirect to home x FullLogoWhite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,13 +9,13 @@ export const Header = () => {
   const scrollToSection = (sectionId) => {
     gsap.to(window, { duration: 1.5, scrollTo: `#${sectionId}`, ease: 'power2.inOut' });
   };
-  
+
   return (
     <header className="header-menu">
         <nav className="header-menu-nav">
           <a onClick={() => scrollToSection('a')} className="menu-element">ABOUT US</a>
           <a onClick={() => scrollToSection('s')} className="menu-element">SERVICES</a>
-          <FullLogoWhite />
+          <FullLogoWhite onClick={() => scrollToSection('h')} className="menu-element"/>
           <a onClick={() => scrollToSection('c')} className="menu-element">CONTACTS</a>
           <a onClick={() => scrollToSection('j')} className="menu-element">JOIN US</a>
         </nav>
