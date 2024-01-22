3257188e22dde148dadbb1f9d6517e221538269a
Fabio <cassisafabio97@gmail.com>
2024-01-22 18:37:10 +0100

    tweaked and improved the title brackets animations

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,31 +1,61 @@
-import React, { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { LeftBracket, RightBracket } from '../assets/SVGassets';
 
-export const AnimatedTitle = ({ text }) => {
-    useEffect(() => {
-        const headerTimeline = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
 
-        // Initially set the left bracket and hide the text
-        headerTimeline.set('.left-bracket', { scaleX: 1, x: '0%', transformOrigin: '0% 50%' })
-                      .set('.right-bracket', { scaleX: 1, x: '-100%', transformOrigin: '100% 50%' })
-                      .set('.title-text', { opacity: 0, y: '100%' });
+export const AnimatedTitle = ({ text, style, leftBracket, rightBracket }) => {
+  const animatedTitleRef = useRef(null);
 
-        // Animation to open the right bracket, revealing the text
-        headerTimeline.to('.right-bracket', { scaleX: 1, x: '0%', duration: 0.5, ease: 'power2.out' })
-                      .to('.title-text', { opacity: 1, y: '0%', duration: 0.5, ease: 'power2.out' }, "+=0.25"); // Starts after the right bracket animation
+  useEffect(() => {
+    const animatedTitle = animatedTitleRef.current;
 
-    }, []);
+    gsap.to(animatedTitle, {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: animatedTitle,
+        start: 'top center', // Adjust as needed
+        end: '+=150', // Adjust as needed
+        toggleActions: 'play none none none',
+        scrub: 1,
+        markers: true, // Setting the markers for testing! TO BE REMOVED.
+        once: true, // Animating only once after scrolling through the page.
+      },
+    });
 
-    return (
-        <div className="animated-title">
-            <div className="left-bracket">
-                <LeftBracket />
-            </div>
-            <div className="title-text">{text}</div>
-            <div className="right-bracket">
-                <RightBracket />
-            </div>
-        </div>
-    );
+    const headerTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: animatedTitle,
+        start: 'top center', // Adjust as needed
+        end: '+=150', // Adjust as needed
+        toggleActions: 'play none none none',
+        once: true, // Animating only once after scrolling through the page.
+      },
+    });
+
+    // Initially set the left bracket and hide the text
+    headerTimeline
+      .set('.left-bracket', { ...leftBracket, transformOrigin: '0% 50%' })
+      .set('.right-bracket', { ...rightBracket, transformOrigin: '0% 50%' })
+      .set('.title-text', { opacity: 0, y: '100%' });
+
+    // Animation to open the right bracket, revealing the text
+    headerTimeline
+      .to('.left-bracket', { scaleX: 1, x: '0%', duration: 1.5, ease: 'power2.out' })
+      .to('.right-bracket', { scaleX: 1, x: '0%', duration: 1.5, ease: 'power2.out' })
+      .to('.title-text', { opacity: 1, y: '0%', duration: 0.5, ease: 'power2.out' }, "+=0.25"); // Starts after the right bracket animation
+  }, [leftBracket, rightBracket]);
+
+  return (
+    <div className="animated-title" style={{ opacity: 0, ...style }} ref={animatedTitleRef}>
+      <div className="left-bracket">
+        <LeftBracket />
+      </div>
+      <div className="title-text">{text}</div>
+      <div className="right-bracket">
+        <RightBracket />
+      </div>
+    </div>
+  );
 };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,12 +7,33 @@ function Landing() {
   return (
     <div className='main-wrapper'>
         <Header />
-        <section id='a' className='about-section'></section>
-        <section id='s' className='services-section'>
-          <AnimatedTitle text={"EQUIPAGGIO"} />
+        <section id='h' className='home-section'></section>
+        <section id='a' className='about-section'>
+          <AnimatedTitle text={"EQUIPAGGIO"} 
+          leftBracket={{ scaleX: 1, x: '0%' }} 
+          rightBracket={{ scaleX: 1, x: '-550%' }}
+          />
         </section>
-        <section id='c' className='contacts-section'></section>
-        <section id='j' className='join-section'></section>
+        <section id='s' className='services-section'>
+          <AnimatedTitle text={"GALASSIE"} 
+          style={{ paddingRight: '0%' }} 
+          leftBracket={{ scaleX: 1, x: '500%' }} 
+          rightBracket={{ scaleX: 1, x: '0%' }} 
+          />
+        </section>
+        <section id='c' className='contacts-section'>
+          <AnimatedTitle text={"CONTACTS"}
+          leftBracket={{ scaleX: 1, x: '0' }} 
+          rightBracket={{ scaleX: 1, x: '-500%' }} 
+          />
+        </section>
+        <section id='j' className='join-section'>
+          <AnimatedTitle text={"JOIN US"} 
+          style={{ paddingRight: '0%' }} 
+          leftBracket={{ scaleX: 1, x: '500%' }} 
+          rightBracket={{ scaleX: 1, x: '0%' }} 
+          />
+        </section>
     </div>
   )
 }
